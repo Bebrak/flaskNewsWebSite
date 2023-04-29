8f047098e599f9df9b00bccf797237e605ccc2bb
BorkinEugen <borkin.zhenya.15@gmail.com>
2023-04-29 20:25:53 +0300

    Added page404.

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ def about():
 def faq():
     return render_template('faq.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title='Страница не найдена')
+
 if __name__ == "__main__":
     app.run()
