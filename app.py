93ea10663f875cc5e8478544f45a63dd82d46464
BorkinEugen <borkin.zhenya.15@gmail.com>
2023-04-30 11:52:31 +0300

    Commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 import os
 from config import Config
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config.from_object(Config)
