a6fa1760d581b9189b508a8beb22c9739ef2a80a
BorkinEugen <borkin.zhenya.15@gmail.com>
2023-05-29 21:16:10 +0300

    1.0

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import schedule
 import datetime
 import os
 import sqlite3
@@ -13,28 +14,31 @@ app.config.update(dict(DATABASE=os.path.join(app.root_path, 'fdb.db')))
 app.permanent_session_lifetime = datetime.timedelta(seconds=60)
 
 def update_news():
-    n_url, n_img = [], []
-    url = 'https://www.bragazeta.ru/'
-    db = connect_db()
-    db = FDataBase(db)
-    response = requests.get(url)
-    bs = BeautifulSoup(response.text, "lxml")
-    n_title = bs.find('div', class_="col-md-6 order-sm-0 order-md-1 oder-lg-1 order-first main-news-article").find_all('h1', class_='title-small')
-    n_text_small = bs.find('div', class_="col-md-6 order-sm-0 order-md-1 oder-lg-1 order-first main-news-article").find_all('p', class_='card-text')
-    for i in bs.find_all('div', class_="media"):
-        n_url.append(i.find('a', href=True)['href'])
-        n_img.append(i.find('img', src=True)['src'])
-    for i in range(len(n_title)):
-        url = n_url[i]
-        n_text_big = ''
+    try:
+        n_url, n_img = [], []
+        url = 'https://www.bragazeta.ru/'
+        db = connect_db()
+        db = FDataBase(db)
         response = requests.get(url)
         bs = BeautifulSoup(response.text, "lxml")
-        lst_text_big = bs.find('div', class_="video-show").find_all('p')
-        for j in lst_text_big:
-            n_text_big += j.text
-            n_text_big += ' '
-        n_text_big = n_text_big.strip()
-        db.addMenu(n_title[i].text, n_text_small[i].text, n_text_big, n_img[i])
+        n_title = bs.find('div', class_="col-md-6 order-sm-0 order-md-1 oder-lg-1 order-first main-news-article").find_all('h1', class_='title-small')
+        n_text_small = bs.find('div', class_="col-md-6 order-sm-0 order-md-1 oder-lg-1 order-first main-news-article").find_all('p', class_='card-text')
+        for i in bs.find_all('div', class_="media"):
+            n_url.append(i.find('a', href=True)['href'])
+            n_img.append(i.find('img', src=True)['src'])
+        for i in range(len(n_title)):
+            url = n_url[i]
+            n_text_big = ''
+            response = requests.get(url)
+            bs = BeautifulSoup(response.text, "lxml")
+            lst_text_big = bs.find('div', class_="video-show").find_all('p')
+            for j in lst_text_big:
+                n_text_big += j.text
+                n_text_big += ' '
+            n_text_big = n_text_big.strip()
+            db.addMenu(n_title[i].text, n_text_small[i].text, n_text_big, n_img[i])
+    except:
+        print('Ошибка при получении данных')
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
@@ -63,7 +67,8 @@ def showNews(id_news):
 @app.route('/about')
 def about():
     return render_template('about.html')
-
+def job():
+    print('bebra')
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
@@ -72,6 +77,9 @@ def faq():
 def page_not_found(error):
     return render_template('page404.html', title='Страница не найдена')
 
+
+schedule.every().hour.do(update_news)
+schedule.run_pending()
+
 if __name__ == "__main__":
-    # update_news() # может работать некорректно из-за обновления на сайте, с которого я беру новости
     app.run()
